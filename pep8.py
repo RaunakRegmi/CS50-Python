585a19710d45d77b6de4105121ed12cc8d7dd0c5
Raunak Regmi <raunak.regmi@gmail.com>
2026-02-06 00:45:48 +0545

    Learnt about PEP8 standard way of writing python code, and editors like black, pylint, pycodestyle.

---
+++ pep8.py
@@ -0,0 +1,14 @@
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Gryffindor",
+    "Padma": "Gryffindor",
+    "Ram": "Gryffindor",
+}
+for student in students:
+    print(student)
+
+
+# pip install black
+# black pep8.py (formats all the things)
