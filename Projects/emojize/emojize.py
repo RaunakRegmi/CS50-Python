a89f9113c8e5eb85949d3dc61c5f01e1d8df0dc5
Raunak Regmi <raunak.regmi@gmail.com>
2026-02-07 01:57:18 +0545

    Made a project, to emojize texts with colon

---
+++ Projects/emojize/emojize.py
@@ -0,0 +1,14 @@
+import emoji
+# import requests
+# import json
+
+user_input = input('Input: ')
+
+print(emoji.emojize(user_input))
+
+# response = requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+#
+# # print(emoji.EMOJI_DATA(user_input))
+#
+# print(json.dumps(response.json()))
+
